59e5d279499269140646e3fd2fc64e9d533fbe56
Mauro Villagra <maurovillagra257@gmail.com>
2023-08-05 02:35:20 -0300

    Termine el trabajo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
-
+import Formulario from './componentes/Formulario';
 function App() {
 
   return (
     <>
-      
+    <Formulario/>
     </>
   )
 }
